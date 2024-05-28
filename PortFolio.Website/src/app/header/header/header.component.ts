1f260ae1a53f65f4c71195b981d1561eeec170c5
Gaurav <gauravojha1111@gmail.com>
2024-05-28 21:14:54 +0530

    PortFolioWebsite

--- PortFolio.Website/src/app/header/header/header.component.ts
+++ PortFolio.Website/src/app/header/header/header.component.ts
@@ -1,25 +1,44 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { GlobalConstant } from '../Constant';
 import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+import Typed from 'typed.js';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [IonicModule],
+  imports: [CommonModule,IonicModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit,OnDestroy {
+  typed:Typed | undefined
+  isDay = true;
   name=GlobalConstant.Name;
   home=GlobalConstant.Home;
   about=GlobalConstant.About;
   work=GlobalConstant.Work;
   contact=GlobalConstant.Contact;
 
+
   constructor(){
 
   }
-  toggleDayNight(){
-    
+  ngOnDestroy(): void {
+    this.typed?.destroy();
+  }
+  ngOnInit(): void {
+    const option={
+      strings: ["Gaurav Ojha","I am Coder","FrontEnd Developer","BackEnd Developer","FullStack Developer"],
+      typeSpeed: 100,
+      backSpeed: 50,
+      loop: true,
+      backDelay:1000
+    }
+    this.typed = new Typed("#text", option);
+  }
+
+  toggleDayNight() {
+    this.isDay = !this.isDay;
   }
 }
